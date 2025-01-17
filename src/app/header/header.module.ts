24f40bef69711def1cffbe7adefdcf415dd65386
Aref Chucri <arefchucri@MacBook-Air-de-Aref.local>
2025-01-17 11:44:49 -0300

    primeiro commit com criacao do module e component

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderProjectComponent } from './header-project/header-project.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderProjectComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderModule { }
